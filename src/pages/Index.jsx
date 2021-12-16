989c48f9363fa1836d78448d49da5e173ca8b7ce
Johnatan Agudelo Múnera <johnatan.fisica@gmail.com>
2021-12-15 22:28:06 -0500

    Agregar codigo base para el front del proyecto

---
+++ src/pages/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Index = () => (
+  <div>
+    <div className='bg-green-300 h-96'>
+      Index page hola estoy desplegando desde gh actions
+    </div>
+    <div className='bg-green-300 h-96'>Index page</div>
+    <div className='bg-green-300 h-96'>Index page</div>
+    <div className='bg-green-300 h-96'>Index page</div>
+  </div>
+);
+
+export default Index;
