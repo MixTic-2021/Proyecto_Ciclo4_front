0052b0452ea9987b5b442ad0dca0e891b13a13de
TheRainHermit <miguel.fabra211294@gmail.com>
2021-12-18 16:25:45 -0500

    Update tailwind.config.js
    Deploy heroku

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      fontFamily: {
-        'sans': ['Helvetica', 'Arial', 'sans-serif'],
-      }
     },
   },
   variants: {
